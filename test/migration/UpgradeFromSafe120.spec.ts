44493fa25d4d85b92df1e86ea47921203b53c002
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-07-24 17:50:37 +0300

    refactor: skip migration tests for zksync

--- test/migration/UpgradeFromSafe120.spec.ts
+++ test/migration/UpgradeFromSafe120.spec.ts
@@ -1,15 +1,15 @@
 import { expect } from "chai";
-import hre, { ethers, deployments, waffle } from "hardhat";
+import hre, { ethers, deployments } from "hardhat";
 import "@nomiclabs/hardhat-ethers";
 import { AddressZero } from "@ethersproject/constants";
-import { getSafeSingleton, getFactory, getMock, getMultiSend } from "../utils/setup";
+import { getSafeSingleton, getFactory, getMock, getMultiSend, getWallets } from "../utils/setup";
 import { buildSafeTransaction, executeTx, safeApproveHash } from "../../src/utils/execution";
 import { verificationTests } from "./subTests.spec";
 import deploymentData from "../json/safeDeployment.json";
 import { calculateProxyAddress } from "../../src/utils/proxies";
 
 describe("Upgrade from Safe 1.2.0", () => {
-    const [user1] = waffle.provider.getWallets();
+    const [user1] = getWallets();
 
     const ChangeMasterCopyInterface = new ethers.utils.Interface(["function changeMasterCopy(address target)"]);
 
