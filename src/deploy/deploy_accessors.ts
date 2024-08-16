e8fa194c397654b6bf195fde55c5f78d6bcb0bdb
Den Smalonski <dzianis.smalonski@protofire.io>
2024-08-16 10:25:38 +0200

    feat: add logic to build sophon

--- src/deploy/deploy_accessors.ts
+++ src/deploy/deploy_accessors.ts
@@ -1,5 +1,6 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+const { ethers } = require("ethers");
 
 const deploy: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment,
@@ -8,11 +9,18 @@ const deploy: DeployFunction = async function (
   const { deployer } = await getNamedAccounts();
   const { deploy } = deployments;
 
+  const baseFeeHex = "0xe9c0154780";
+  const baseFeePerGas = ethers.BigNumber.from(baseFeeHex);
+  const maxPriorityFeePerGas = ethers.utils.parseUnits("2", "gwei"); // 2 Gwei
+  const maxFeePerGas = baseFeePerGas.add(maxPriorityFeePerGas);
+
   await deploy("SimulateTxAccessor", {
     from: deployer,
     args: [],
     log: true,
     deterministicDeployment: true,
+    maxFeePerGas: maxFeePerGas,
+    maxPriorityFeePerGas: maxPriorityFeePerGas,
   });
 };
 
